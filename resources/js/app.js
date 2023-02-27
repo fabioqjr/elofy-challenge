36a71f7085540ca38c4bce3fceb1bb4a87d99890
Fabio Quintana <fabioqjr@gmail.com>
2023-02-27 04:57:04 -0300

    front init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,21 @@
-import './bootstrap';
+import "./bootstrap";
+import $ from "jquery";
+window.$ = $;
 
-console.log('imported');
+$(document).ready(function () {
+    fetch("/listar-exemplo")
+        .then((response) => response.json())
+        .then((json) => {
+            json.forEach((item) => {
+                console.log(item);
+                $("[js-host-data]").append(`
+                    <div>
+                        <br>
+                        <p>${item.name}</p>
+                        <p>${item.description}</p>
+                        <br>
+                    </div>
+                `);
+            });
+        });
+});
--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Exemplo</title>
+    @vite('resources/sass/app.scss')
 </head>
 <body>
-    <h1>Apenas um exemplo</h1>
+    <h1>/listar-exemplo</h1>
 
-    <script src="/js/app.js"></script>
+    <div js-host-data></div>
+
+    @vite('resources/js/app.js')
 </body>
 </html>
